0601e620c74bf16fece6b3b994b2dd4370f7a327
Aleksey Pekar <fenix_al@tut.by>
2023-09-24 14:19:17 +0300

    rest api

---
+++ 14-rest-api/src/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import router from './router';
+
+const app = express();
+
+app.use(
+    cors({
+        credentials: true,
+    }),
+);
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(8080, () => {
+    console.log("Server running on http://localhost:8080");
+});
+
+const MONGO_URL =
+    "mongodb+srv://fenixAl:fenixAl@cluster0.lotc9os.mongodb.net/api?retryWrites=true&w=majority";
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use('/', router());
